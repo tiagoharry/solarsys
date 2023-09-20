f2291dbaac60e6e1c274fd228cac7c0f3b4ea3fd
Tiago Assis <tiagoassisandrade@hotmail.com>
2023-09-20 15:18:56 -0300

    animate list option mobile

--- js/main.js
+++ js/main.js
@@ -98,3 +98,19 @@ function modal() {
     modal.close()
   }
 }
+
+
+let listElements = document.querySelectorAll('.link');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        if (listElement.classList.contains('active')){
+            listElement.classList.remove('active');
+        }else{
+            listElements.forEach (listE => {
+                listE.classList.remove('active');
+            })
+            listElement.classList.toggle('active');
+        }
+    })
+});
